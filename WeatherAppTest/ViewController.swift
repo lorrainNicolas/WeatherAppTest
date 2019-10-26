39146693908f2f7d0ce7609b182bf41751be4137
Nicolas Lorrain <nicolas.lorrain@withings.com>
2019-10-26 08:30:40 +0200

    Initial Commit

---
+++ WeatherAppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppTest
+//
+//  Created by Nicolas Lorrain on 26/10/2019.
+//  Copyright © 2019 Nicolas Lorrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
